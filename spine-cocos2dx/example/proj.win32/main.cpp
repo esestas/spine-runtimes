af989e392c6e8dcd3d178bfa111f84a9066cfc86
badlogic <badlogicgames@gmail.com>
2016-06-20 14:31:25 +0200

    New cocos2dx example project, for Windows, Linux and Mac OS X. Doesn't work on Windows/Linux yet

--- spine-cocos2dx/example/proj.win32/main.cpp
+++ spine-cocos2dx/example/proj.win32/main.cpp
@@ -1,22 +1,18 @@
 #include "main.h"
-#include "../Classes/AppDelegate.h"
+#include "AppDelegate.h"
+#include "cocos2d.h"
 
 USING_NS_CC;
 
 int APIENTRY _tWinMain(HINSTANCE hInstance,
-					   HINSTANCE hPrevInstance,
-					   LPTSTR lpCmdLine,
-					   int nCmdShow)
+                       HINSTANCE hPrevInstance,
+                       LPTSTR    lpCmdLine,
+                       int       nCmdShow)
 {
-	UNREFERENCED_PARAMETER(hPrevInstance);
-	UNREFERENCED_PARAMETER(lpCmdLine);
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
 
-	// create the application instance
-	AppDelegate app;
-
-	auto director = Director::getInstance();
-	auto glview = GLViewImpl::create("Spine Example");
-	glview->setFrameSize(960, 640);
-	director->setOpenGLView(glview);
-	return Application::getInstance()->run();
+    // create the application instance
+    AppDelegate app;
+    return Application::getInstance()->run();
 }
